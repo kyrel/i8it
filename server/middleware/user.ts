3de98b46622bc837752f22f765aa66a68f9ba987
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2024-06-30 00:54:22 +0200

    Don't access supabase session server-side

--- server/middleware/user.ts
+++ server/middleware/user.ts
@@ -5,8 +5,8 @@ export default defineEventHandler(async (event) => {
   // This is the slow method of getting the server user,
   // but the fast one (involving session) doesn't work after the session get expired
   // This code requires a revisit
-  const client = serverSupabaseClient(event);
-  const session = await client.auth.getSession();
+  //const client = serverSupabaseClient(event);
+  //const session = await client.auth.getSession();
   //console.log(session);
   const user = await serverSupabaseUser(event);
   event.context.user = user; //session.data.session?.user;
